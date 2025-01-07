e32653b1da3859b44492568cd6af0129f713de16
renato3x <pereirarenato21@gmail.com>
2025-01-07 19:07:43 -0300

    databse: changed to prisma

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,5 @@ import 'reflect-metadata';
 import 'dotenv/config';
 
 import { server } from './server';
-import { dataSource } from '@database/data-source';
-import { logger } from '@logger';
 
-dataSource.initialize()
-.then(() => {
-  logger.info('Server connected to the database')
-  server();
-})
-.catch((error) => {
-  logger.error('Unexpected error at connecting to database', error);
-});
+server();
