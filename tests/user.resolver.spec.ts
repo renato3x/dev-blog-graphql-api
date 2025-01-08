b4b7f85ffa6cbdedd5650bfadcfc29aeaf171153
renato3x <pereirarenato21@gmail.com>
2025-01-08 09:56:15 -0300

    setup: remove @app path

--- tests/user.resolver.spec.ts
+++ tests/user.resolver.spec.ts
@@ -1,7 +1,7 @@
 import 'reflect-metadata';
 import 'dotenv/config';
 
-import { server } from '@app/server';
+import { server } from '../src/server';
 import { beforeAll, describe, expect, test } from 'vitest';
 
 let apolloServer: Awaited<ReturnType<typeof server>>;
